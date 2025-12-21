52c521029b8f3858e63017f902a065d6221867de
Arunponugoti <arun.devop1@gmail.com>
2025-12-21 13:54:24 +0530

    2th iteration

--- fullstack-app/backend/app.py
+++ fullstack-app/backend/app.py
@@ -2,7 +2,11 @@ from flask import Flask, jsonify
 import psycopg2
 import os
 
+
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 def get_db_connection():
     conn = psycopg2.connect(
